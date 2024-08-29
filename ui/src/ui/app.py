e3c16149a6998b1ad09b2ae919973eb883dc262a
Vijay Kiran <mail@vijaykiran.com>
2024-08-29 11:15:00 +0200

    first cut of htmx ui

--- ui/src/ui/app.py
+++ ui/src/ui/app.py
@@ -1,7 +1,14 @@
 from pydantic import UUID4
 from dataclasses import dataclass
 
-from litestar import Litestar, get, post
+from litestar.contrib.htmx.request import HTMXRequest
+from litestar.contrib.htmx.response import HTMXTemplate
+from litestar import get, Litestar, post
+
+from litestar.response import Template
+from litestar.template.config import TemplateConfig
+from litestar.contrib.jinja import JinjaTemplateEngine
+from pathlib import Path
 
 checks = []
 
@@ -12,9 +19,8 @@ class Check:
     type: str
 
 
-@get("/checks/{check_id:uuid}")
+@get("/api/checks/{check_id:uuid}")
 async def get_check(check_id: UUID4) -> dict[str, int]:
-    # find checks by id
     for check in checks:
         if check.id == check_id:
             return {"check_id": check.id, "check_type": check}
@@ -22,10 +28,21 @@ async def get_check(check_id: UUID4) -> dict[str, int]:
             return {"check_id": "not found"}
 
 
-@post("/checks")
+@post("/api/checks")
 async def create_check(data: Check) -> dict[str, str]:
     checks.append(data)
     return {"check_id": data.id, "check_type": data.type}
 
 
-app = Litestar([create_check, get_check])
+@get(path="/")
+async def index(request: HTMXRequest) -> Template:
+    htmx = request.htmx
+    return HTMXTemplate(template_name="index.html")
+
+
+app = Litestar(route_handlers=[index, create_check, get_check],
+               request_class=HTMXRequest,
+               template_config=TemplateConfig(
+                   directory=Path(__file__).parent / "templates",
+                   engine=JinjaTemplateEngine
+               ))
