8ce8f36b87f9b6df44dbe5c89f13d44d56baf786
Vasu Gaur <vasu.gaur@nl.abnamro.com>
2024-08-29 11:54:56 +0200

    some rules

--- engine/fast_check/run_check.py
+++ engine/fast_check/run_check.py
@@ -1,18 +1,24 @@
-from helper import AzureFileHandler
+import pandas as pd
+from pprint import pprint
 
-azure_handler = AzureFileHandler(
-    spn_app_id="", spn_password="", tenant_id="", account_url=""
-)
-CONTAINER = ""
+def run_check(check_type, column: str, values, data: pd.DataFrame)-> tuple[pd.DataFrame, pd.DataFrame]:
+    # check_types = ['in_list', 'values_in_range']
+    if check_type=='in_list':
+        query = data[column].isin(values)
+    elif check_type=='values_in_range':
+        query = data[column].between(values[0], values[1])
+    elif check_type=='not_null':
+        query = data[column].isnull()
+    return (data[query], data[~query])
 
+def get_columns(dataset_path: str)->dict:
+    df = pd.read_csv(dataset_path)    
+    return {'error_code':0, 'columns': list(df.columns)}
 
-def execute_check(check_id: str, check_json: dict, dataset_location: str) -> dict:
+def execute_check(column_name: str, check_type, values, dataset_path: str) -> dict:
     """
     load the dataset from the passed location using azure library.
     """
-    ret_dict = dict()
-    data_csv = azure_handler.read_file_from_container_azure(
-        container=CONTAINER, file_path=dataset_location
-    )
-
-    return ret_dict
+    df = pd.read_csv(dataset_path)    
+    hits, passing = run_check(check_type, column_name, values, df)
+    return {'error_code': 0, 'data': {'hits': hits.values.tolist(), 'passing': passing.values.tolist()}}
---
+++ engine/tests/test_checks.py
@@ -0,0 +1,4 @@
+from fast_check.run_check import read_file_as_csv
+
+# def test_read_file():
+#     read_file_as_csv
